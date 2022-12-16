0a6df775a25556603331de069bd68e3ad84e482e
evanmalherbe <evanmalherbe@gmail.com>
2022-12-16 09:14:36 +0200

    Add project files.

---
+++ Extensibility/ConsoleLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    public class ConsoleLogger : ILogger
+    {
+        void ILogger.LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        void ILogger.LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Extensibility/DbMigrator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at: " + DateTime.Now);
+
+            // Details of migrating db
+
+            _logger.LogInfo("Migration stopped at: " + DateTime.Now);
+            
+        }
+    }
+}
